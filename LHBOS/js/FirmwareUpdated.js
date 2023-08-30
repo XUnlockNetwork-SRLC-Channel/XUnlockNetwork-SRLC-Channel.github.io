9a7eaeb11e1de3203905d3b8117a55cf3d027599
XCader <100986647+luacoder279@users.noreply.github.com>
2023-08-30 14:49:49 -0700

    Update FirmwareUpdated.js

--- LHBOS/js/FirmwareUpdated.js
+++ LHBOS/js/FirmwareUpdated.js
@@ -5,14 +5,14 @@ function notifyUpdted() {
   } else if (Notification.permission === "granted") {
     // Check whether notification permissions have already been granted;
     // if so, create a notification
-    const notification = new Notification("Firmware Changes:Cookies added so that you wont lose game data");
+    const notification = new Notification("Firmware Changes:Added Cluster Rush");
     // …
   } else if (Notification.permission !== "denied") {
     // We need to ask the user for permission
     Notification.requestPermission().then((permission) => {
       // If the user accepts, let's create a notification
       if (permission === "granted") {
-        const notification = new Notification("Firmware Changes:Cookies added so that you wont lose game data");
+        const notification = new Notification("Firmware Changes:Added Cluster Rush");
         // …
       }
     });
