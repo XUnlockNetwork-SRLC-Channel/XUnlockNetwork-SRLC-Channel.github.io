ac686cd2f1d73fe9750716ac8306203ff6640618
XCader <100986647+luacoder279@users.noreply.github.com>
2023-09-01 09:34:47 -0700

    Update SystemGrabber.js

--- LHBOS/js/Prefrences/SystemGrabber.js
+++ LHBOS/js/Prefrences/SystemGrabber.js
@@ -1,1 +1,13 @@
 navigator.getBattery()
+  .then(function(battery) {
+    (function init() {;
+      updateLevelInfo();
+
+
+      battery.addEventListener('levelchange', updateLevelInfo);
+                          function updateLevelInfo() {
+ 
+      let perc = battery.level * 100;
+    document.getElementById("battery").innerHTML = (perc.toFixed(2) + '%');
+
+    }]
